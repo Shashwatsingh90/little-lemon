6f7621f6b15fc809ce10d3463c17733aa4858ef5
T.C. Cox <thmscly@gmail.com>
2023-02-24 16:26:14 -0500

    added logo to Nav and content to Footer

--- little-lemon/src/components/Footer.js
+++ little-lemon/src/components/Footer.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer></footer>
+        <footer>
+            <h3>Doormat Navigation</h3>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Reservations</a></li>
+                <li><a href="">Order Online</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+            <h3>Contact</h3>
+            <ul>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+            <h3>Social Media Links</h3>
+            <ul>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+                <li><a href="">LinkedIn</a></li>
+            </ul>
+        </footer>
     )
 }
 
--- little-lemon/src/components/Nav.js
+++ little-lemon/src/components/Nav.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import logo from '../icons_assets/Logo.svg'
 
 function Nav() {
     return (
-        <nav></nav>
+        <nav>
+            <img src={logo} alt="" />
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Reservations</a></li>
+                <li><a href="">Order Online</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
     )
 }
 

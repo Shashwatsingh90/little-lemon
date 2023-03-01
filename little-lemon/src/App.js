f8c85d3e3167469dd260faa8ff181c0ce4deaee5
T.C. Cox <thmscly@gmail.com>
2023-03-01 14:03:28 -0500

    updated routes in App.js

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import BookingPage from './components/BookingPage'
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import About from './components/About';
+import OrderOnline from './components/OrderOnline';
+import Menu from './components/Menu';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />}></Route>
           <Route path='/booking' element={<BookingPage />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/order' element={<OrderOnline />}></Route>
+          <Route path='/menu' element={<Menu />}></Route>
+          <Route path='/login' element={<Login />}></Route>
         </Routes>
       </main>
     </div>

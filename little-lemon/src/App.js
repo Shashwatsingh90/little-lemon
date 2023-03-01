7ad88f1120453963accee659c70bf209e15568fd
T.C. Cox <thmscly@gmail.com>
2023-03-01 13:18:26 -0500

    added initial routes to index/app.js

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -4,10 +4,16 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Nav from './components/Nav';
+import BookingPage from './components/BookingPage'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+        <Route path='/booking' element={<BookingPage />}></Route>
+      </Routes>
       <Nav />
       <Header />
       <Main />

a3b889cde6f2fd907f56560cf23df5c32f7e3df8
T.C. Cox <thmscly@gmail.com>
2023-03-28 10:58:22 -0400

    created ConfirmedBooking page | created pages folder

---
+++ little-lemon/src/pages/ConfirmedBooking.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ConfirmedBooking() {
+  return (
+    <div>
+      <p>Your booking has been confirmed!</p>
+    </div>
+  );
+}
+
+export default ConfirmedBooking;
